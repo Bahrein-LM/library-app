03f1bc1785b65f077b4d5d01051ce6bbe5a8e21d
Bahrein-LM <bahrein.giri@gmail.com>
2025-01-23 21:30:11 +0700

    initial commit

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react'
+
+const Page = () => {
+  return (
+    <main className='root-container flex min-h-screen flex-col items-center justify-center'>
+        <h1 className='font-bebas-neue text-5xl font-bold text-light-600'>Whoa, Slow Down Kiddo, What do you want to rush for!!</h1>
+        <p className='max-w-xl mt-3 text-center text-light-400'>
+            Looks like you&apos;ve been a little too eager. We&apos;ve put a temporary pause on your excitement. 🧘Chill for a bit, and try again shortly
+        </p>
+    </main>
+  )
+}
+
+export default Page
